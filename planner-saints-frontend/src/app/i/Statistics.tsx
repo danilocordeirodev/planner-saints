f116dbca91b629c617ebd06133d6840ff0dc856f
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-05-19 18:25:35 -0300

    Add basic layout

--- planner-saints-frontend/src/app/i/Statistics.tsx
+++ planner-saints-frontend/src/app/i/Statistics.tsx
@@ -1,3 +1,30 @@
+'use client'
+
+import Loader from "@/components/ui/Loader"
+import { useProfile } from "@/hooks/userProfile"
+
 export default function Statistics() {
-  return <div>Statistics</div>
+
+  const { data, isLoading } = useProfile()
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <div className='grid grid-cols-4 gap-12 mt-7'>
+      {data?.statistics.length ? (
+        data.statistics.map(statistic => (
+          <div
+            className='bg-border/5 rounded p-layout text-center
+            hover:-translate-y-3 transition-transform duration-500'
+          >
+            <div className='text-xl'>{statistic.label}</div>
+            <div className='text-3xl font-semibold'>{statistic.value}</div>
+          </div>
+        ))
+      ) : (
+        <div>Statistics not loaded!</div>
+      )}
+      
+    </div>
+  )
 }
